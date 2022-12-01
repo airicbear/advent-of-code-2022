9edb7d29020b53eecc3f5f51e27f214ba802b124
Eric Nguyen <tuk94307@temple.edu>
2022-12-01 01:45:03 -0500

    Day 1 Part 2

--- 01/Puzzle01.java
+++ 01/Puzzle01.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Scanner;
 
 class Puzzle01 {
@@ -10,7 +11,8 @@ class Puzzle01 {
     public static void main(String[] args) {
         if (args.length > 0) {
             ElfListReader elfListReader = new ElfListReader(args[0]);
-            System.out.println(elfListReader.getElfList().elfWithMostCalories());
+            System.out.println(elfListReader.getTotalCaloriesOfElfWithMostCalories());
+            System.out.println(elfListReader.getTotalCaloriesOfTopThreeElvesWithMostCalories());
         } else {
             System.err.println(ERROR_ARGUMENT_NOT_FOUND);
         }
@@ -37,7 +39,7 @@ class ElfListReader {
                     elfList.addNewElf();
                 } else {
                     Food food = new Food(Integer.parseInt(line));
-                    elfList.lastElf().addFood(food);
+                    elfList.getLastElf().addFood(food);
                 }
             }
 
@@ -47,8 +49,19 @@ class ElfListReader {
         }
     }
 
-    public ElfList getElfList() {
-        return elfList;
+    public int getTotalCaloriesOfElfWithMostCalories() {
+        return elfList.getMaxElf().getCalories();
+    }
+    
+    public int getTotalCaloriesOfTopThreeElvesWithMostCalories() {
+        List<Elf> maxElves = elfList.getMaxElves(3);
+
+        int total = 0;
+        for (int i = 0; i < 3; i++) {
+            total += maxElves.get(i).getCalories();
+        }
+
+        return total;
     }
 }
 
@@ -67,26 +80,53 @@ class ElfList {
         elves.add(new Elf());
     }
 
-    public Elf lastElf() {
-        if (elves.size() > 0) {
-            return elves.get(elves.size() - 1);
+    public int getSize() {
+        return elves.size();
+    }
+
+    public Elf getElf(int index) {
+        return elves.get(index);
+    }
+
+    public Elf getLastElf() {
+        if (getSize() > 0) {
+            return getElf(elves.size() - 1);
         } else {
             return null;
         }
     }
 
-    public int elfWithMostCalories() {
-        int maxCalories = 0;
-
-        for (int i = 0; i < elves.size(); i++) {
-            Elf elf = elves.get(i);
-
-            if (elf.getTotalCaloriesOfSupplies() > maxCalories) {
-                maxCalories = elf.getTotalCaloriesOfSupplies();
-            }
+    public List<Elf> sortedElves() {
+        List<Elf> sorted = new ArrayList<>(elves);
+        sorted.sort(Comparator.comparing(Elf::getCalories).reversed());
+        return sorted;
+    }
+    
+    public List<Elf> getMaxElves(int n) {
+        if (n < 1) {
+            return new ArrayList<>();
+        } else if (n > getSize()) {
+            n = getSize();
         }
 
-        return maxCalories;
+        List<Elf> maxElves = new ArrayList<>();
+        List<Elf> sorted = sortedElves();
+
+        for (int i = 0; i < n; i++) {
+            maxElves.add(sorted.get(i));
+        }
+
+        return maxElves;
+    }
+
+    public Elf getMaxElf() {
+        List<Elf> maxElves = getMaxElves(1);
+
+        if (maxElves.isEmpty()) {
+            return null;
+        } else {
+            return maxElves.get(0);
+        }
     }
 }
 
@@ -97,7 +137,7 @@ class Elf {
         this.supplies = new ArrayList<>();
     }
 
-    public int getTotalCaloriesOfSupplies() {
+    public int getCalories() {
         int total = 0;
 
         for (int i = 0; i < supplies.size(); i++) {
