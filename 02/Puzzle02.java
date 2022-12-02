79d7e85b16acce8ad11464e74b43c4eed067807f
Eric Nguyen <tuk94307@temple.edu>
2022-12-02 00:51:51 -0500

    Day 2 Part 1

---
+++ 02/Puzzle02.java
@@ -0,0 +1,152 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Puzzle02 {
+    private static final String ERROR_ARGUMENT_NOT_FOUND = "ERROR: Filename not provided.\nUSAGE: java Puzzle01 [filename].";
+
+    public static void main(String[] args) {
+        
+        if (args.length > 0) {
+            StrategyListReader strategyListReader = new StrategyListReader(args[0]);
+            System.out.println(strategyListReader.getStrategyScore());
+        } else {
+            System.err.println(ERROR_ARGUMENT_NOT_FOUND);
+        }
+    }
+}
+
+class StrategyListReader {
+    private StrategyList strategyList;
+
+    public StrategyListReader(String filename) {
+        File file = new File(filename);
+        strategyList = new StrategyList();
+
+        try {
+            Scanner s = new Scanner(file);
+
+            while (s.hasNextLine()) {
+                String line = s.nextLine();
+                String[] tokens = line.split(" ");
+
+                Move opponent = Move.parse(tokens[0]);
+                Move you = Move.parse(tokens[1]);
+                Match match = new Match(opponent, you);
+
+                strategyList.addMatch(match);
+            }
+
+            s.close();
+        } catch (FileNotFoundException e) {
+            System.err.println("ERROR: File \"" + filename + "\" not found.");
+        }
+    }
+
+    public int getStrategyScore() {
+        return strategyList.getTotalScore();
+    }
+}
+
+class StrategyList {
+    private List<Match> matches;
+
+    public StrategyList() {
+        this.matches = new ArrayList<>();
+    }
+
+    public void addMatch(Match match) {
+        matches.add(match);
+    }
+
+    private List<Result> getResults() {
+        List<Result> results = new ArrayList<>();
+        for (Match match : matches) {
+            results.add(match.judge());
+        }
+        return results;
+    }
+
+    public int getTotalScore() {
+        List<Result> results = getResults();
+        int total = 0;
+        for (int i = 0; i < matches.size(); i++) {
+            int resultScore = results.get(i).getValue();
+            int moveScore = matches.get(i).getB().getWeight();
+            total += resultScore + moveScore;
+        }
+        return total;
+    }
+}
+
+class Match {
+    private Move a;
+    private Move b;
+
+    public Match(Move a, Move b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public Result judge() {
+        if (a == b) {
+            return Result.DRAW;
+        }
+        if ((a == Move.ROCK && b == Move.PAPER)
+            || (a == Move.PAPER && b == Move.SCISSORS)
+            || (a == Move.SCISSORS && b == Move.ROCK)) {
+            return Result.WIN;
+        }
+        return Result.LOSE;
+    }
+
+    public Move getB() {
+        return b;
+    }
+}
+
+enum Result {
+    LOSE(0), DRAW(3), WIN(6);
+
+    private int value;
+
+    Result(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
+
+enum Move {
+    ROCK(1), PAPER(2), SCISSORS(3);
+
+    private int weight;
+
+    Move(int weight) {
+        this.weight = weight;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public static Move parse(String s) {
+        switch (s) {
+            case "A":
+            case "X":
+                return Move.ROCK;
+            case "B":
+            case "Y":
+                return Move.PAPER;
+            case "C":
+            case "Z":
+                return Move.SCISSORS;
+            default:
+                return Move.ROCK;
+        }
+    }
+}
