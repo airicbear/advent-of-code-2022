26848ffdc6073a0fdd4e215b58886dfa85a30743
Eric Nguyen <tuk94307@temple.edu>
2022-12-03 12:01:56 -0500

    Day 3 Part 2

--- 03/Puzzle03.java
+++ 03/Puzzle03.java
@@ -12,6 +12,7 @@ public class Puzzle03 {
         if (args.length > 0) {
             RucksackList rucksackList = RucksackList.parse(args[0]);
             System.out.println(rucksackList.sumCommonPriorities());
+            System.out.println(rucksackList.sumGroupPriorities(3));
         } else {
             System.err.println("ERROR: Filename not provided.\nUSAGE: java Puzzle03 [filename].");
         }
@@ -50,9 +51,51 @@ class RucksackList {
     public int sumCommonPriorities() {
         int total = 0;
         for (Rucksack rucksack : rucksacks) {
-            total += rucksack.compare();
+            total += rucksack.findCommonItem();
         }
         return total;
+    }
+
+    public List<Group> groups(int n) {
+        List<Group> groups = new ArrayList<>();
+        for (int i = 0; i < rucksacks.size(); i += n) {
+            Group group = Group.create();
+            for (int j = 0; j < n; j++) {
+                group.add(rucksacks.get(i + j));
+            }
+            groups.add(group);
+        }
+        return groups;
+    }
+
+    public int sumGroupPriorities(int n) {
+        int total = 0;
+        for (Group group : groups(n)) {
+            total += group.findBadge().getPriority();
+        }
+        return total;
+    }
+}
+
+class Group {
+    private List<Rucksack> rucksacks;
+
+    public static Group create() {
+        Group group = new Group();
+        group.rucksacks = new ArrayList<>();
+        return group;
+    }
+
+    public void add(Rucksack rucksack) {
+        rucksacks.add(rucksack);
+    }
+
+    public Item findBadge() {
+        Set<Item> initialSet = rucksacks.get(0).itemSet();
+        for (int i = 1; i < rucksacks.size(); i++) {
+            initialSet.retainAll(rucksacks.get(i).itemSet());
+        }
+        return initialSet.iterator().next();
     }
 }
 
@@ -71,10 +114,17 @@ class Rucksack {
         return rucksack;
     }
 
-    public int compare() {
+    public int findCommonItem() {
         Compartment intersection = Compartment.intersection(left, right);
         Item commonItem = intersection.itemIterator().next();
         return commonItem.getPriority();
+    }
+
+    public Set<Item> itemSet() {
+        Set<Item> leftSet = new HashSet<>(left.getItems());
+        Set<Item> rightSet = new HashSet<>(right.getItems());
+        leftSet.addAll(rightSet);
+        return leftSet;
     }
 }
 
@@ -83,6 +133,10 @@ class Compartment {
 
     public Iterator<Item> itemIterator() {
         return items.iterator();
+    }
+
+    public List<Item> getItems() {
+        return items;
     }
 
     public static Compartment intersection(Compartment left, Compartment right) {
